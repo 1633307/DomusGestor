514a3ee8c44aa45c4e6540f166dd29370dd3e60e
Pol Pedro <ppedro@bertia.es>
2026-05-12 20:55:38 +0200

    fix: add has_perm and has_module_perms to Usuari model

--- backend/users/models.py
+++ backend/users/models.py
@@ -35,6 +35,12 @@ class Usuari(AbstractBaseUser):
     def __str__(self):
         return self.username
 
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
 
 class InfoImmobiliaria(models.Model):
     nom_comercial   = models.CharField(max_length=100)
