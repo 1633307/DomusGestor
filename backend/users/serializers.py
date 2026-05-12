a36b11f18871fcecda90297f641ffcf2b2865bf7
Pol Pedro <ppedro@bertia.es>
2026-05-12 20:56:22 +0200

    feat: update serializers - add UsuariSerializer with is_admin/date_joined, CreateUsuariSerializer and UpdateUsuariSerializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,22 +1,18 @@
 from rest_framework import serializers
-
 from .models import Usuari, InfoImmobiliaria
 
 
 class LoginSerializer(serializers.Serializer):
-    nip = serializers.CharField()
+    nip      = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        nip = data.get('nip')
-        password = data.get('password')
-
         try:
-            user = Usuari.objects.get(nip=nip)
+            user = Usuari.objects.get(nip=data['nip'])
         except Usuari.DoesNotExist:
             raise serializers.ValidationError('Credencials incorrectes.')
 
-        if not user.check_password(password):
+        if not user.check_password(data['password']):
             raise serializers.ValidationError('Credencials incorrectes.')
 
         if not user.is_active:
@@ -28,16 +24,45 @@ class LoginSerializer(serializers.Serializer):
 
 class UsuariSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Usuari
-        fields = ['id', 'username', 'email', 'nip']
-        read_only_fields = ['id']
+        model  = Usuari
+        fields = [
+            'id', 'username', 'first_name', 'last_name',
+            'email', 'nip', 'is_admin', 'is_active', 'date_joined',
+        ]
+        read_only_fields = ['id', 'date_joined']
+
+
+class CreateUsuariSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model  = Usuari
+        fields = ['username', 'first_name', 'last_name', 'email', 'nip', 'password', 'is_admin']
+
+    def validate_nip(self, value):
+        if Usuari.objects.filter(nip=value).exists():
+            raise serializers.ValidationError("Ja existeix un usuari amb aquest NIP.")
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = Usuari(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UpdateUsuariSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = Usuari
+        fields = ['username', 'first_name', 'last_name', 'email', 'nip', 'is_admin']
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
 
     class Meta:
-        model = Usuari
+        model  = Usuari
         fields = ['username', 'email', 'nip', 'password']
 
     def validate_nip(self, value):
@@ -55,5 +80,5 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 class InfoImmobiliariaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = InfoImmobiliaria
+        model  = InfoImmobiliaria
         fields = '__all__'
